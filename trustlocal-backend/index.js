ee655c25c94fbefe11aab0a94379e1170de5ec24
AbhishekSahu077511 <abhisheksahu077511@gmail.com>
2026-03-31 19:59:14 +0530

    final updated

--- trustlocal-backend/index.js
+++ trustlocal-backend/index.js
@@ -15,26 +15,38 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: "*", // Testing ke liye sabhi origins allow hain
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"] // Explicitly Authorization allow kiya
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/bookings', bookingRoutes);
-app.use('/api/admin', adminRoutes);
+app.use('/auth', authRoutes);
+app.use('/bookings', bookingRoutes);
+app.use('/admin', adminRoutes);
 
 app.get('/', (req, res) => {
   res.send('TrustLocal API is running... 🚀');
 });
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI)
-  .then(() => {
-    console.log('✅ MongoDB Connected to Database');
-    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-  })
-  .catch((err) => console.log('❌ MongoDB Connection Error:', err));
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "trustLocal"
+    });
+    console.log('✅ MongoDB Connected');
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
+
+  } catch (err) {
+    console.error('❌ MongoDB Connection Error:', err);
+    process.exit(1);
+  }
+};
+
+// function call
+connectDB();
